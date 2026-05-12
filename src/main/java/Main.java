f5867447e292416f75ebf0118e7d62a0fdc25e32
aleksLegat <aleks-deuce@yandex.ru>
2026-05-12 16:06:19 +0300

    Первый проект. Гонки 24

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,28 +14,41 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Создадим переменную для регистрации участников гонки
-        Car[] listOfParticipants = new Car[3];
+        //Car[] listOfParticipants = new Car[3];
 
         // Регистрация участников
         // Можно сразу при регистрации участников определить победителя,
         // как советует Artur, но тогда пропадает необходимость в классе Race
         // который вроде как нужен по условиям задачи
+        Car leader = null;
+        int maxDistance = 0;
+
         for (int i = 0; i < 3; i++) {
             int numberOfParticipant = i + 1;
             System.out.println(String.format("Введите имя %d участника", numberOfParticipant));
             String currentName = scanner.next();
+            int currentSpeed = inputSpeed(scanner, numberOfParticipant);
 
-            listOfParticipants[i] = new Car(currentName, inputSpeed(scanner, numberOfParticipant));
+            Car currentCar = new Car(currentName, currentSpeed);
+            int currentDistance = currentCar.getDistance(racingTime);
+
+            if (leader == null) {
+                leader = currentCar;
+                maxDistance = currentDistance;
+            } else {
+                if (maxDistance < currentDistance) {
+                    leader = currentCar;
+                    maxDistance = currentDistance;
+                }
+            }
         }
 
-        Race race24 = new Race(listOfParticipants, racingTime);
-        Car leader = race24.getLader();
         System.out.println("В гонке победил автомобиль " + leader.name);
     }
 
     // Вынес код ввода скорости в отдельную функцию
     public static int inputSpeed(Scanner scanner, int numberOfParticipant) {
-        int currentSpeed = 0;
+        int currentSpeed = -1;
         while (currentSpeed < minSpeed || currentSpeed > maxSpeed) {
             System.out.println(String.format("Введите скорость автомобиля %d участника в диапазоне от %d до %d км/ч", numberOfParticipant, minSpeed, maxSpeed));
             currentSpeed = scanner.nextInt();
