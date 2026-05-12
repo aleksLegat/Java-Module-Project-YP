3bcbfbcac5e5d46ec4450cf1d9ed454c3f814718
aleksLegat <aleks-deuce@yandex.ru>
2026-05-12 22:08:34 +0300

    Первый проект. Гонки 24. Новый вариант 2.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,15 +8,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // При регистрации участников сразу вычисдяем победителя гонок
+        // При регистрации участников сразу вычисляем победителя гонок
         Car leader = null;
         int maxDistance = 0;
 
+
+
         for (int i = 0; i < 3; i++) {
             int numberOfParticipant = i + 1;
+
             System.out.println("Введите имя " + numberOfParticipant + " участника");
-            String currentName = scanner.next();
+            String currentName = "";
+            while ((currentName = scanner.nextLine()).trim().isEmpty()) {
+               System.out.println("Вы ввели некорректное имя участника!");
+            }
             int currentSpeed = inputSpeed(scanner, numberOfParticipant);
+            scanner.nextLine();
 
             Car currentCar = new Car(currentName, currentSpeed);
             int currentDistance = currentCar.getDistance(racingTime);
@@ -33,15 +40,29 @@ public class Main {
         }
 
         System.out.println("В гонке победил автомобиль " + leader.name);
+        scanner.close();
     }
 
     // Вынес код ввода скорости в отдельную функцию
     public static int inputSpeed(Scanner scanner, int numberOfParticipant) {
         int currentSpeed = -1;
-        while (currentSpeed < minSpeed || currentSpeed > maxSpeed) {
-            System.out.println("Введите скорость автомобиля " + numberOfParticipant + " участника в диапазоне от " + minSpeed + " до " + maxSpeed + " км/ч");
-            currentSpeed = scanner.nextInt();
+        System.out.println("Введите скорость автомобиля " + numberOfParticipant + " участника в диапазоне от " + minSpeed + " до " + maxSpeed + " км/ч");
+        while (true) {
+            if (!scanner.hasNextInt()) {
+                System.out.println("Вы ввели что-то не то!");
+                // что-то без этого уходит в бесконечный цикл, наверное нужно ввод как-то прочитать
+                scanner.nextLine();
+            } else {
+                currentSpeed = scanner.nextInt();
+                if (currentSpeed <= minSpeed || currentSpeed >= maxSpeed) {
+                    System.out.println("Введите значение скорости в диапазоне от " + minSpeed + " до " + maxSpeed + " км/ч");
+                    scanner.nextLine();
+                } else {
+                    break;
+                }
+            }
         }
+
         return currentSpeed;
     }
 }
