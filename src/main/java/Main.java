64ff7a9ac13487c409006161829e56c794082607
aleksLegat <aleks-deuce@yandex.ru>
2026-05-12 13:49:55 +0300

    Первый проект. Гонки 24

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,31 @@
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        // Создадим переменную для регистрации участников гонки
+        Car[] listOfParticipants = new Car[3];
+
+        // Регистрация участников
+        for (int i = 0; i < 3; i++) {
+            int numberOfParticipant = i + 1;
+            System.out.println(String.format("Введите имя %d участника", numberOfParticipant));
+            String currentName = scanner.next();
+
+            // Наверное нужно как то учесть уникальность имен участников
+            // но вроде нет такого в условиях задачи
+
+            int currentSpeed = 0;
+            while (currentSpeed <= 0 || currentSpeed > 250) {
+                System.out.println(String.format("Введите скорость автомобиля %d участника в диапазоне от 1 до 250 км/ч", numberOfParticipant));
+                currentSpeed = scanner.nextInt();
+            }
+            listOfParticipants[i] = new Car(currentName, currentSpeed);
+        }
+
+        Race race24 = new Race(listOfParticipants);
+        Car leader = race24.getLader();
+        System.out.println("В гонке победил автомобиль " + leader.name);
     }
 }
---
+++ src/main/java/Race.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.HashMap;
+
+public class Race {
+    Car[] participants;
+    Race(Car[] participants) {
+        this.participants = participants;
+    }
+
+    Car getLader() {
+        HashMap<Car, Integer> currentRace = new HashMap<>();
+        // Рассчитаем растояние, которое пройдет каждый автомобиль за 24 часа
+        // и засунем все это в ассоциативный массив, где ключ - автомобиль а значение - пройденное расстояние за 24 часа
+        // в принципе можно было и не рассчитывать расстояние, а отсортировать просто по скорости
+        for (Car ourCar : participants) {
+            currentRace.put(ourCar, ourCar.getDistance(24));
+        }
+        // тут бы просто отсортировать по значению, но сортировку вроде как не проходили
+        Car leader = participants[0];
+        int maxDistance = currentRace.get(leader);
+        for (int i = 1; i < 3; i++) {
+            Car currentCar = participants[i];
+            int currentDistance = currentRace.get(currentCar);
+            if (maxDistance < currentDistance) {
+                maxDistance = currentDistance;
+                leader = currentCar;
+            }
+        }
+        return leader;
+    }
+}
