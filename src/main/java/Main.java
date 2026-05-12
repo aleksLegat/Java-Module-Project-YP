8b800097fc7a8ca879dc4e2553761e80b32af64d
aleksLegat <aleks-deuce@yandex.ru>
2026-05-12 15:16:31 +0300

    Первый проект. Гонки 24

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,30 +2,45 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
+    // директиву final и константы мы еще в рамках курса не проходили
+    // но не могу не воспользоваться советом куратора )
+    // вообще весь код можно было сделать гораздо проще, но хочется
+    // попробовать разные возможности языка
+    public static final int minSpeed = 1;
+    public static final int maxSpeed = 250;
+    public static final int racingTime = 24;
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Создадим переменную для регистрации участников гонки
         Car[] listOfParticipants = new Car[3];
 
         // Регистрация участников
+        // Можно сразу при регистрации участников определить победителя,
+        // как советует Artur, но тогда пропадает необходимость в классе Race
+        // который вроде как нужен по условиям задачи
         for (int i = 0; i < 3; i++) {
             int numberOfParticipant = i + 1;
             System.out.println(String.format("Введите имя %d участника", numberOfParticipant));
             String currentName = scanner.next();
 
-            // Наверное нужно как то учесть уникальность имен участников
-            // но вроде нет такого в условиях задачи
-
-            int currentSpeed = 0;
-            while (currentSpeed <= 0 || currentSpeed > 250) {
-                System.out.println(String.format("Введите скорость автомобиля %d участника в диапазоне от 1 до 250 км/ч", numberOfParticipant));
-                currentSpeed = scanner.nextInt();
-            }
-            listOfParticipants[i] = new Car(currentName, currentSpeed);
+            listOfParticipants[i] = new Car(currentName, inputSpeed(scanner, numberOfParticipant));
         }
 
-        Race race24 = new Race(listOfParticipants);
+        Race race24 = new Race(listOfParticipants, racingTime);
         Car leader = race24.getLader();
         System.out.println("В гонке победил автомобиль " + leader.name);
     }
+
+    // Вынес код ввода скорости в отдельную функцию
+    public static int inputSpeed(Scanner scanner, int numberOfParticipant) {
+        int currentSpeed = 0;
+        while (currentSpeed < minSpeed || currentSpeed > maxSpeed) {
+            System.out.println(String.format("Введите скорость автомобиля %d участника в диапазоне от %d до %d км/ч", numberOfParticipant, minSpeed, maxSpeed));
+            currentSpeed = scanner.nextInt();
+        }
+        return currentSpeed;
+    }
 }
+
--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -2,25 +2,29 @@ import java.lang.reflect.Array;
 import java.util.HashMap;
 
 public class Race {
-    Car[] participants;
-    Race(Car[] participants) {
+    final Car[] participants;
+    final int racingTime;
+    Race(Car[] participants, int racingTime) {
         this.participants = participants;
+        this.racingTime = racingTime;
     }
 
     Car getLader() {
-        HashMap<Car, Integer> currentRace = new HashMap<>();
+        //HashMap<Car, Integer> currentRace = new HashMap<>();
         // Рассчитаем растояние, которое пройдет каждый автомобиль за 24 часа
         // и засунем все это в ассоциативный массив, где ключ - автомобиль а значение - пройденное расстояние за 24 часа
         // в принципе можно было и не рассчитывать расстояние, а отсортировать просто по скорости
-        for (Car ourCar : participants) {
-            currentRace.put(ourCar, ourCar.getDistance(24));
-        }
+        //for (Car ourCar : participants) {
+        //    currentRace.put(ourCar, ourCar.getDistance(24));
+        //}
         // тут бы просто отсортировать по значению, но сортировку вроде как не проходили
         Car leader = participants[0];
-        int maxDistance = currentRace.get(leader);
+        //int maxDistance = currentRace.get(leader);
+        int maxDistance = leader.getDistance(racingTime);
         for (int i = 1; i < 3; i++) {
             Car currentCar = participants[i];
-            int currentDistance = currentRace.get(currentCar);
+            //int currentDistance = currentRace.get(currentCar);
+            int currentDistance = currentCar.getDistance(racingTime);
             if (maxDistance < currentDistance) {
                 maxDistance = currentDistance;
                 leader = currentCar;
